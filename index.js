fe0dc4a5e3d2c6fa321218bf1bc533a9c0a4a609
David Martin <dmarti21@gmail.com>
2014-02-26 10:26:08 -0500

    test

--- index.js
+++ index.js
@@ -4,6 +4,49 @@ var myApp = angular.module('myApp',[ 'GithubRepoFetcher', 'RepoFetcherMeta', 'Re
 //myApp.factory('myService', function() {});
 
 function RepoCtrl($scope, GithubRepo, qChain, RepoMeta, $q) {
+  var cfg = $scope.cfg = {};
+  cfg.panelDescriptionLength = 40;
+
+  cfg.progressClass = function(repo){
+    console.log('progress class', repo._ff_meta_ && repo._ff_meta_.progress);
+    var name = repo._ff_meta_ && repo._ff_meta_.progress;
+    if (name) {
+      return getChildNames(name)[0].replace(/\s/g, "-");
+    }
+    return name || "";
+  };
+
+
+  var rateLimit = $scope.rateLimit = {};
+
+  function getChildNames(obj){
+    console.log(obj);
+    if (!obj) {
+      return [];
+    }
+
+    if (!Object.keys(obj).length ) {
+      return [];
+    }
+    var name = Object.keys(obj).map(function(k){ return k.toString(); })
+    console.log('child name', name);
+    return name;
+  }
+
+  //side affect function
+  function getRateLimits(repos){
+    // Stinky - see parent module for details
+    var headers = GithubRepo.headers();
+    rateLimit.fromCache = headers['X-Local-Cache'];
+
+    var remaining = headers['x-ratelimit-remaining'];
+    var reset = headers['x-ratelimit-reset'];
+    rateLimit.remaining = remaining && parseInt(remaining);
+    rateLimit.resetTime = reset && moment.unix( parseInt(reset)).fromNow();
+
+    return repos;
+
+  }
 
   function setSelectedRepos(repos){
     $scope.selectedRepos = repos;
@@ -30,21 +73,24 @@ function RepoCtrl($scope, GithubRepo, qChain, RepoMeta, $q) {
 
   $scope.name = 'forforf';
 
-  var initFilters = [
-    {sort: 'updated', per_page: 5}
-  ];
-  //var initFilters = [];
-
-  var baseFilters = [
-    slicerFn(0,100)
-  ];
 
 
-  $scope.fetch = function(){
-    //var allFilters = initFilters.concat(baseFilters);
+  $scope.fetch = function(apiFetchLimit){
+    apiFetchLimit = apiFetchLimit || 100;
+
+    var initFilters = [
+      {sort: 'updated', per_page: apiFetchLimit}
+    ];
+    //var initFilters = [];
+
+    var baseFilters = [
+      slicerFn(0,8)
+    ];
+
+    var allFilters = initFilters.concat(baseFilters);
 
     //debug
-    var allFilters = initFilters;
+    //var allFilters = initFilters;
     // -- debug
     var creds = {username: $scope.name, password: $scope.pw};
     initFromRepo = GithubRepo.fetcher(creds, allFilters, {init: true});
@@ -53,7 +99,9 @@ function RepoCtrl($scope, GithubRepo, qChain, RepoMeta, $q) {
 
 
 
+
     initFromRepo
+      .then(getRateLimits)
       .then(setSelectedRepos)
       .then(addRepoMeta)
     ;
